afb85dc567eea379e511c09ba1be090907c990a4
prdpta <pradiptaputramei01@gmail.com>
2024-04-25 18:03:16 +0700

    commit 7

--- 037_singlelinkedlist.cpp
+++ 037_singlelinkedlist.cpp
@@ -49,3 +49,12 @@ void addNode() {
     nodeBaru->next = current;
     previous->next = nodeBaru;
 }
+
+bool searchNode(int nim, Node* current, Node* previous) {
+    previous = START;
+    current = START;
+    while (current != NULL && nim > current->noMhs)
+    {
+        previous = current;
+        current = current->next;
+    }
