028b8e7fd4c7c209983031cf63c6097bd9483b2d
prdpta <pradiptaputramei01@gmail.com>
2024-04-25 18:10:32 +0700

    commit 14

--- 037_singlelinkedlist.cpp
+++ 037_singlelinkedlist.cpp
@@ -113,3 +113,18 @@ void searchData() {
         system("cls");
         return;
     }
+    else {
+        int nim;
+        cout << "Masukan NIM: ";
+        cin >> nim;
+        Node* currentNode = START;
+        while (currentNode->noMhs == nim) {
+            if (currentNode->noMhs == nim) {
+                cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name <<endl;
+                return;
+            }
+            currentNode = currentNode->next;
+        }
+        cout << "Data tidak ditemukan" << endl;
+    }
+}
