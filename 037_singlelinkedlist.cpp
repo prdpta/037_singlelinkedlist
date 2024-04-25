b316c8c11eb20d885149358c73a65d30ab870223
prdpta <pradiptaputramei01@gmail.com>
2024-04-25 18:13:34 +0700

    commit 17

--- 037_singlelinkedlist.cpp
+++ 037_singlelinkedlist.cpp
@@ -151,3 +151,11 @@ int main() {
                 system("pause");
                 system("cls");
                 break;
+            case 2:
+                if (listEmpty())
+                {
+                    cout << "List kosong" << endl;
+                    system("pause");
+                    system("cls");
+                    break;
+                }
