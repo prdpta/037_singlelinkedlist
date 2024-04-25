a07574cf83effa07e16d28de809630232a75daf2
prdpta <pradiptaputramei01@gmail.com>
2024-04-25 17:58:44 +0700

    commit 3

--- 037_singlelinkedlist.cpp
+++ 037_singlelinkedlist.cpp
@@ -6,3 +6,16 @@ struct Node {
     string name;
     Node* next;
 };
+
+Node* START = NULL;
+
+void addNode() {
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukan NIM: ";
+    cin >> nim;
+    cout << "Masukan Nama: ";
+    cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
